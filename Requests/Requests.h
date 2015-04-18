5140aa5a0a8a7bfe6accced29a68704f854fe50a
Daniel Duan <daniel@duan.org>
2015-04-18 14:00:56 -0700

    Switched to Xcode project

---
+++ Requests/Requests.h
@@ -0,0 +1,19 @@
+//
+//  Requests.h
+//  Requests
+//
+//  Created by Daniel Duan on 4/18/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Requests.
+FOUNDATION_EXPORT double RequestsVersionNumber;
+
+//! Project version string for Requests.
+FOUNDATION_EXPORT const unsigned char RequestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Requests/PublicHeader.h>
+
+
---
+++ RequestsTests/RequestsTests.swift
@@ -0,0 +1,16 @@
+//
+//  RequestsTests.swift
+//  RequestsTests
+//
+//  Created by Daniel Duan on 4/18/15.
+//
+//
+
+import UIKit
+import XCTest
+import Requests
+
+class RequestsTests: XCTestCase {
+
+    
+}
