d128d640604701c1fe22e9f285e7bc6bfe3d8734
Daniel Duan <daniel@duan.org>
2015-10-22 21:55:51 -0700

    added tvOS target

---
+++ Just-tvOS/Just-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Just-tvOS.h
+//  Just-tvOS
+//
+//  Created by Daniel Duan on 10/22/15.
+//  Copyright © 2015 JustHTTP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Just-tvOS.
+FOUNDATION_EXPORT double Just_tvOSVersionNumber;
+
+//! Project version string for Just-tvOS.
+FOUNDATION_EXPORT const unsigned char Just_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Just_tvOS/PublicHeader.h>
+
+
---
+++ Just/Just.h
@@ -0,0 +1,19 @@
+//
+//  Just.h
+//  Just
+//
+//  Created by Daniel Duan on 10/22/15.
+//  Copyright © 2015 JustHTTP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Just.
+FOUNDATION_EXPORT double JustVersionNumber;
+
+//! Project version string for Just.
+FOUNDATION_EXPORT const unsigned char JustVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Just/PublicHeader.h>
+
+
