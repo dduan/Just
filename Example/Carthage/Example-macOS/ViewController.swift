620196441fe4d3b1e7dae4b9a91c6a05a3ca6e98
Daniel Duan <daniel@duan.org>
2016-10-09 01:35:29 -0700

    trim test source files

--- Example/Carthage/Example-macOS/ViewController.swift
+++ Example/Carthage/Example-macOS/ViewController.swift
@@ -9,20 +9,5 @@
 import Cocoa
 import Just
 
-class ViewController: NSViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    // Do any additional setup after loading the view.
-  }
-
-  override var representedObject: Any? {
-    didSet {
-    // Update the view, if already loaded.
-    }
-  }
-
-
-}
+class ViewController: NSViewController {}
 
