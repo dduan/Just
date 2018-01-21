9c13bd2cc7c4cc09719a7246847a9459cb45aaf2
Tyler Milner <tylermilner@gmail.com>
2018-01-21 13:59:40 -0800

    Support for previous commit. Implemented Package.swift for Swift 4.0.

--- Package.swift
+++ Package.swift
@@ -2,4 +2,14 @@
 
 import PackageDescription
 
-let package = Package(name: "Just")
+let package = Package(
+    name: "Just",
+    products: [
+        .library(name: "Just", targets: ["Just"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Just", dependencies: []),
+        .testTarget(name: "JustTests", dependencies: ["Just"])
+    ]
+)
